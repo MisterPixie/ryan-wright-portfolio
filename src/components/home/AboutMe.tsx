78ba04493a0bb125956a5ccf29a926f43ddf9ef2
MisterPixie <ryanwright812@gmail.com>
2022-05-09 21:37:31 +0100

    Push

--- src/components/home/AboutMe.tsx
+++ src/components/home/AboutMe.tsx
@@ -2,18 +2,27 @@ export const AboutMe = () => {
     return (
         <div className="aboutMe">
             <div className="container">
-                <div className="columns">
+                <div className="columns v-centered centered">
                     <div className="column is-narrow">
                         <figure className="image is-128x128">
                             <img src="https://bulma.io/images/placeholders/256x256.png"/>
                         </figure>
                     </div>
                     <div className="column">
-                        <div className="title">
-                            <p>About Me:</p>
-                        </div>
-                        <div className="description">
-                            <p>Blah Blah</p>
+                        <div className="content is-desktop is-vcentered">
+                            <div className="title">
+                                <h5>About Me</h5>
+                            </div>
+                            <div className="description">
+                                <p>
+                                    My life as a programmer began at the young age of 14 when I learned programming fundamentals which unlocked a massive world of unlimited possibilities.
+                                    <br/>
+                                    After I finished school, I was a sole trader learning lots of different skills such as multilanguage programming, networking, back-end/front-end development & volunteer management.
+                                    <br/><br/>
+                                    To this current day, I work as a Software Developer for Track a Parcel, developing logistics software that will change the world. I'm also a Cub Scout Leader because I believe in volunteering & helping people learn key life skills.
+                                    I'm Currently Learning React, React Native, Swift in my spare time
+                                </p>
+                            </div>
                         </div>
                     </div>
                 </div>
