d6902525b1fa05a1bf7c1f9873f62d28ece0b265
MisterPixie <ryanwright812@gmail.com>
2022-05-14 19:51:58 +0100

    Init Langauges file with some added extras

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,10 +1,12 @@
 import AboutMe from "../components/home/AboutMe";
 import ContactMe from "../components/home/ContactMe";
+import Languages from "../components/home/Languages";
 
 export const App = () => {
     return (
         <div className="app">
             <AboutMe/>
+            <Languages/>
             <ContactMe/>
         </div>
     )
